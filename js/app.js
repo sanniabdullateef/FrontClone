ae3d287bb2601874ab0222f716d83f18f39f16f5
Emmax <aneriemmax@gmail.com>
2019-11-17 10:35:46 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const typed = new Typed('#intro-typed', {
+  strings: ['more professional', 'perfect in every way', 'astonishing'],
+  typeSpeed: 40,
+  backSpeed: 40,
+  startDelay: 1000,
+  loop: true
+})
